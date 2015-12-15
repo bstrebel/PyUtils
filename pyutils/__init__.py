ea0db19151c17eff8bcf013addd57172f9b93840
Bernd Strebel (E6530) <bernd.strebel@googlemail.com>
2015-12-15 16:36:47 +0100

    Beta 0.1.2

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -1,14 +1,19 @@
 import os
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __license__ = 'GPL2'
 __author__ = 'Bernd Strebel'
 
 PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-from .logutils import LogAdapter
+from .logutils import LogAdapter, LogFileHandler
+from .timeutils import strflocal
+
 
 __all__ = [
 
-    'LogAdapter'
+    'LogAdapter',
+    'LogFileHandler',
+    'strflocal'
+
 ]
