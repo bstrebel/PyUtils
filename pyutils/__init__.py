4a5920b76dccaec59995dcc385e45039d78fdb8b
Bernd Strebel <b.strebel@digitec.de>
2016-01-22 11:09:05 +0100

    convert millisecond timestamps

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -1,6 +1,6 @@
 import os, sys, logging
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __license__ = 'GPL2'
 __author__ = 'Bernd Strebel'
 
--- pyutils/timeutils.py
+++ pyutils/timeutils.py
@@ -6,15 +6,15 @@ import os, sys, time
 def strflocal(tm=None, format='%Y-%m-%d %H:%M:%S'):
 
     if not tm:
-        tm = time.time() * 1000
+        tm = time.time()
 
-    # try:
-    #     # epoch value in seconds
-    #     lt = time.localtime(tm)
-    # except ValueError:
-    #     # epoch value in milliseconds
-    #     lt = time.localtime(tm/1000)
+    # convert floating point to int/long
+    tm = int(tm)
 
-    lt = time.localtime(tm/1000)
+    if len(str(tm)) == 13:
+        # concvert milliseconds to seconds
+        tm = tm/1000
+
+    lt = time.localtime(tm)
 
     return time.strftime(format, lt)
